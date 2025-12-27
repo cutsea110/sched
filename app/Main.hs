0de21a7e3970233771e3077569cdf0ded22de1d1
cutsea110 <cutsea110@gmail.com>
2025-12-27 12:04:38 +0900

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -18,26 +18,15 @@ quad n = n `mod` 4 == 0
 
 
 dayN'sWork :: Unit -> Int -> [[Unit]]
-dayN'sWork unitMax n = map (filter pred) $ cycle1:cycle2:cycle3:cycle4:cycle5:cycle6:cycle7:cycle8:cycle9:cycle10:cycle11:cycle12:cycle13:cycle14:cycle15:cycle16:cycle17:cycle18:[]
+dayN'sWork unitMax n = map (filter pred) $ map cycle [1..18]
   where pred m = 0 < m && m <= unitMax
-        cycle1  = [n]
-        cycle2  = [n-1]
-        cycle3  = [n-4]
-        cycle4  = [n-9]
-        cycle5  = [n-17]
-        cycle6  = [n-29]
-        cycle7  = [n-46]
-        cycle8  = if odd (n-67) then [n-67,n-66] else []
-        cycle9  = if odd (n-88) then [n-88,n-87] else []
-        cycle10 = if odd (n-109) then [n-109,n-108] else []
-        cycle11 = if quad (n-129) then [n-132,n-131,n-130,n-129] else []
-        cycle12 = if quad (n-155) then [n-158,n-157,n-156,n-155] else []
-        cycle13 = if quad (n-182) then [n-185,n-184,n-183,n-182] else []
-        cycle14 = if quad (n-209) then [n-212,n-211,n-210,n-209] else []
-        cycle15 = if quad (n-236) then [n-239,n-238,n-237,n-236] else []
-        cycle16 = if quad (n-263) then [n-266,n-265,n-264,n-263] else []
-        cycle17 = if quad (n-290) then [n-293,n-292,n-291,n-290] else []
-        cycle18 = if quad (n-317) then [n-320,n-319,n-318,n-317] else []
+        cycle i | 1 <= i && i <= 7  = let a = diff1 !! (i-1) in [n-a]
+                | 8 <= i && i <= 10 = let a = diff2 !! (i-8) in if odd (n-a) then [n-a, n-a+1] else []
+                | 11 <= i           = let a = diff4 !! (i-11) in if quad (n-a) then [n-a-3,n-a-2,n-a-1, n-a] else []
+        diff1 = [0,1,4,9,17,29,46]
+        diff2 = [67,88,109]
+        diff4 = 129:[155,182..]
+               
 
 
 daysFromDay :: Day -> Int -> [Day]
