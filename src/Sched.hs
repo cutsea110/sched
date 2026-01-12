1303045078b78e88fa3f2e8f4946e696018c64f4
cutsea110 <cutsea110@gmail.com>
2026-01-12 16:20:03 +0900

    refactor

--- src/Sched.hs
+++ src/Sched.hs
@@ -11,10 +11,10 @@ workOn unitMax repetition n = map (filter valid . unitsAt) [1..repetition]
   where valid :: Int -> Bool
         valid m = 0 < m && m <= unitMax
         unitsAt :: Int -> [Unit]
-        unitsAt i | 1 <= i && i <= 7  = let u = n - diff1 !! (i-1)  in [u]
-                  | 8 <= i && i <= 10 = let u = n - diff2 !! (i-8)  in if odd u then [u, u+1] else []
-                  | 11 <= i           = let u = n - diff4 !! (i-11) in if by4 u then [u-3, u-2, u-1, u] else []
-                  | otherwise         = error "unexpected index in rule"
+        unitsAt i | 1 <= i && i < 8  = let u = n - diff1 !! (i-1)  in [u]
+                  | 8 <= i && i < 11 = let u = n - diff2 !! (i-8)  in if odd u then [u, u+1] else []
+                  | 11 <= i          = let u = n - diff4 !! (i-11) in if by4 u then [u-3, u-2, u-1, u] else []
+                  | otherwise        = error "unexpected index in rule"
           where
             by4 :: Int -> Bool
             by4 x = x `mod` 4 == 0
